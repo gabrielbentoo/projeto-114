3a0572e4c7c598d0cfa60f057c70d5a449665073
gabrielbentoo <115742572+gabrielbentoo@users.noreply.github.com>
2023-07-17 20:27:27 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    
+
+  canvas = createCanvas(350,350);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(200,200);
+  video.hide();
+
+
+
+}
+
+
+
+
+
+function draw()
+{
+
+}
+function onClick()
+{
+    
+}
